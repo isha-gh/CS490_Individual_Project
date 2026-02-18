4936b2b009a050c895919bf7405cb3e97f17b6f6
Manisha Palaniappan <manisha@Manishas-MacBook-Pro.local>
2026-02-17 21:59:17 -0500

    db fixed

--- backend/db.py
+++ backend/db.py
@@ -4,6 +4,6 @@ def get_db():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="$topthistra1n",
+        password="PASSWORD",
         database="sakila"
     )
